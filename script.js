31707d1c08af2f10914e2700c2d8021bec8e29e8
Yash <sagardyyash19@gmail.com>
2021-11-02 15:22:31 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,66 @@
+let hr = 0;
+let mn = 0;
+let sec = 0;
+let count = 0;
+let timer = false;
+
+let hrr = document.getElementById("hr");
+let min = document.getElementById("min");
+let sc = document.getElementById("sec");
+let cnt = document.getElementById("count");
+
+
+function Start() {
+        if (!timer) {
+                timer = true;
+                stopWatch();
+        }
+}
+
+function Stop() {
+        timer = false;
+}
+
+function Reset() {
+        timer = false;
+        hr = 0;
+        mn = 0;
+        sec = 0;
+        count = 0;
+        hrr.innerHTML = "00";
+        min.innerHTML = "00";
+        sc.innerHTML = "00";
+        cnt.innerHTML = "00";
+}
+
+function stopWatch() {
+
+        if (timer == true) {
+                count = count + 1;
+                cnt.innerHTML = (count < 10) ? `0${count}` : count;
+
+                if (count == 100) {
+                        sec += 1;
+                        count = 0;
+                        sc.innerHTML = (sec < 10) ? `0${sec}` : sec;
+                }
+
+                if (sc == 60) {
+                        min += 1;
+                        sec = 0;
+                        min.innerHTML = (min < 10) ? `0${min}` : min;
+                }
+
+                if (min == 60) {
+                        hr += 1;
+                        min = 0;
+                        hrr.innerHTML = (hr < 10) ? `0${hr}` : hr;
+                }
+
+
+
+                setTimeout(() => {
+                        stopWatch();
+                }, 10)
+        }
+}
